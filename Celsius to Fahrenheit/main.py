e0d77c0708fe74f527e6cd59cd32b05b3f0b56e3
yenesis-rabelo <yenesis.rabelo@ucas-edu.net>
2024-09-26 09:08:17 -0600

    Finished Celsius to Fahrenheit test

---
+++ Celsius to Fahrenheit/main.py
@@ -0,0 +1,7 @@
+#hello world
+
+celsius = input("whats is the temperature in celsius for today")
+
+fahrenheit = (float(celsius) *9/5)+32
+
+print("When it is "+celsius+" degrees celsius it is "+str(fahrenheit)+" degrees fahrenheit")
