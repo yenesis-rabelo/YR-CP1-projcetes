71dab5457242e4c7c9e3b512b55708462d3645f4
yenesis-rabelo <yenesis.rabelo@ucas-edu.net>
2024-09-27 09:42:05 -0600

    Graded Quiz

---
+++ Graded Quiz/main.py
@@ -0,0 +1,18 @@
+#Yenesis Rabelo Graded Quiz
+score=0
+question1=input("is paris in france:")
+if question1 == "yes":
+    score += 1 
+question2=input("are apples red:")
+if question2 == "yes":
+    score += 1
+question3=input("is 2+2=4:")
+if question3 == "yes":
+    score += 1 
+question4=input("is 5+2=7:")
+if question4 == "yes":
+    score += 1  
+question5=input("is 7+2=9:")
+if question5 == "yes":
+    score += 1  
+print("your final score is", score)
