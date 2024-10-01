4a36e95b95e5d34db7e312f538bd137ba39aa90a
yenesis-rabelo <yenesis.rabelo@ucas-edu.net>
2024-10-01 09:18:19 -0600

    Madilb Assignment

---
+++ Madilb/main.py
@@ -0,0 +1,14 @@
+#Yenesis Rabelo Madilb
+
+noun_1=input("noun ")
+noun_2=input("noun ")
+verb_1=input("a past tense verb ")
+name_1=input("name ")
+noun_3=input("noun ")
+adjective_1=input("adjective ")
+verb_2=input("a past tense verb ")
+place_1=input("place ")
+print("""the """+noun_1+""" fell from the """+noun_2+""" and """+verb_1+""" """+name_1+"""'s """+noun_3+""" 
+"""+name_1+""" was """+adjective_1+""" at the """+noun_2+""" so """+name_1+""" """+verb_2+""" all the """+noun_1+""" down.
+"""+name_1+""" then took all the """+noun_1+""" """+place_1+""" to make """+noun_1+""" pie.
+"""+name_1+""" and """+name_1+"""'s family enjoyed a nice meal.""")
