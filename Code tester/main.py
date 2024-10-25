0f798354c1e0676556fdfab3c5ea6c4390e6de2a
yenesis-rabelo <yenesis.rabelo@ucas-edu.net>
2024-10-25 09:27:50 -0600

    Casting

---
+++ Code tester/main.py
@@ -0,0 +1,8 @@
+
+# This function is supposed to add an item to a list, but it produces unexpected results. Find the bug.
+
+def add_to_list(item, my_list=[]):
+
+     my_list.append(item)
+
+     return my_list
---
+++ casting/main.py
@@ -0,0 +1,18 @@
+#Yenesis Rabelo Casting Test
+name = input("what is your name?")
+age = input("what is your age?")
+height = input("what is your height in meters")
+favorite_number =  input("what is your favorite number?")
+print("your name is"+ name)
+print("your age"+ age)
+print("your height"+ height)
+print("your favorite number"+ favorite_number)
+
+age = int(age)
+favorite_number= int(favorite_number)
+height = float(height)
+
+print("your name is" + str(name))
+print("your age"+  str(age))
+print("your height"+ str (height))
+print("your favorite number"+ str (favorite_number))
